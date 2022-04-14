074c9b7e46cefa24e4f8432d9e4b117729ff7ee1
94dreamer <503633021@qq.com>
2022-04-15 02:14:23 +0800

    feat: 初始化

---
+++ renderer.js
@@ -0,0 +1,106 @@
+const skipChanglogLabel = ['skip-changelog'];
+const fixLabel = ['fix', 'bug', 'hotfix']
+const breakingLabel = ['breaking', 'breaking changes']
+const featureLabel = ['feature', 'feat', 'enhancement']
+
+const Renderer = {
+    getPRformtNotes: (body) => {
+        const reg = /in\shttps\:\/\/github\.com\/.+\/pull\/(\d+)\s/g;
+
+        const arr = [...body.matchAll(reg)];
+
+        return arr.map(n => n[1]); // pr number list
+    },
+    regToPrObj: (arr) => {
+        return {
+            cate: arr[1],
+            component: arr[2],
+            desc: arr[3],
+        }
+    },
+    renderCate: (cate) => {
+        return `${cate.sort().map(pr => {
+            const title = pr.changlog ? `\`${pr.changlog.component}\`: ${pr.changlog.desc}` : pr.title
+            return title + ` [@${pr.user.login}](https://github.com/${pr.user.login}) ((#${pr.number})[${pr.html_url}])`
+        }).join('\n')}`
+    },
+    renderMarkdown: (pullRequestList) => {
+        // 分类靠标签
+        // 标题看有没有更新日志
+        const categories = {
+            breaking: [],
+            features: [],
+            bugfix: [],
+            extra: []
+        }
+
+        pullRequestList.forEach(pr => {
+            pr.body = pr.body ? pr.body : '';
+
+            // 不需要纳入 Changelog 的 label
+            if (pr.labels.find(l => skipChanglogLabel.indexOf(l.name) !== -1)) {
+                console.log('pr ' + pr.number + ' 有skipChanglogLabel')
+                return
+            }
+            // 在 pr body 明确填了 跳过 label
+            if (pr.body.indexOf('[x] 本条 PR 不需要纳入 Changelog') !== -1) {
+                console.log('pr ', pr.number, ' 显示不需要纳入 Changelog')
+                return
+            }
+
+            if (pr.body.indexOf('### 📝 更新日志') !== -1) {
+
+                const reg = /-\s([A-Za-z]+)\(([A-Za-z]+)\)\:\s(.+)/g
+
+                const arr = [...pr.body.matchAll(reg)];
+
+                if (arr.length === 0) {
+                    console.log('没有找到任何一条日志内容', pr.number, pr.body);
+                    categories.extra.push(pr);
+                    return
+                }
+
+                arr.map(a => Renderer.regToPrObj(a)).forEach(changlog => {
+                    const logItem = {
+                        ...pr,
+                        changlog
+                    }
+
+                    function isInLabel(label) {
+                        return label.indexOf(changlog.cate) !== -1 || (arr.length === 1 && pr.labels.some(l => label.indexOf(l.name) !== -1))
+                    }
+
+                    if (isInLabel(breakingLabel)) {
+                        categories.breaking.push(logItem)
+                    } else if (isInLabel(featureLabel)) {
+                        categories.features.push(logItem)
+                    } else if (isInLabel(fixLabel)) {
+                        categories.bugfix.push(logItem)
+                    } else {
+                        categories.extra.push(logItem)
+                    }
+                })
+            } else {
+                // 说明开发者没有按模版填写 pr，默认取 title
+                console.log('pr ', pr.number, ' 没有填写模版')
+                categories.extra.push(pr);  //??
+            }
+        });
+
+        return [
+            categories.breaking.length ?
+                `### ❗ Breaking Changes
+${Renderer.renderCate(categories.breaking)}` : '',
+
+            categories.features.length ? `### 🚀 Features
+${Renderer.renderCate(categories.features)}` : '',
+
+            categories.bugfix.length ? `### 🐞 Bug Fixes
+${Renderer.renderCate(categories.bugfix)}` : '',
+
+            categories.extra.length ? `### 🚧 Others
+${Renderer.renderCate(categories.extra)}` : ''].join('\n')
+    }
+}
+
+module.exports = Renderer;
